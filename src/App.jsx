5e06bb80247d06a2198ba5cb02dd406e9ea27e5e
Steven TAING <taing.steven@outlook.fr>
2024-06-17 18:13:56 +0200

    Convert to react

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,72 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 // CSS
 import "./App.css";
 
 function App() {
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+  const photoRef = useRef(null);
 
   const [userDevices, setUserDevices] = useState([]);
   const [deviceSelect, setDeviceSelect] = useState();
   const [currentDevice, setCurrentDevice] = useState();
+  const [streaming, setStreaming] = useState(false);
 
   let width = 320;
   let height = 0;
-  let streaming = false;
-  let video = null;
-  let canvas = null;
-  let photo = null;
-  let startButton = null;
 
   // Charge les cameras de l'utilisateur
   useEffect(() => {
     getUserFlux();
-  },[])
+  }, []);
 
   // Genere les options pour la selection des cameras
   useEffect(() => {
     userDevices.length != 0 && displayDevices();
-  }, [userDevices])
+  }, [userDevices]);
 
   // Charge le flux video en fonction de la camera choisie
   useEffect(() => {
-    startup()
-  }, [currentDevice])
+    startup();
+  }, [currentDevice]);
 
   const getUserFlux = async () => {
     const devices = await navigator.mediaDevices.enumerateDevices();
     setUserDevices(devices.filter((device) => device.kind == "videoinput"));
-  }
+  };
 
-  const displayDevices = () => (
-    setDeviceSelect(userDevices.map((device, index) => (<option key={index} id={device.deviceId}>{device.label}</option>)))
-  );
+  const displayDevices = () =>
+    setDeviceSelect(
+      userDevices.map((device, index) => (
+        <option key={index} id={device.deviceId}>
+          {device.label}
+        </option>
+      ))
+    );
 
   const startup = async () => {
-    video = document.getElementById("video");
-    canvas = document.getElementById("video-canvas");
-    photo = document.getElementById("detection-canvas");
-    startButton = document.getElementById("screenshot-button");
-    
-   try{ const stream = currentDevice === undefined ?
-    await navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-    : await  navigator.mediaDevices.getUserMedia({ video: { deviceId: { exact: currentDevice } } })
-    video.srcObject = stream;
-    video.play();
-  }catch(e){
-    console.log(e.message);
-  }
-  
-  video.addEventListener("canplay", () => {
+    try {
+      const stream =
+        currentDevice === undefined
+          ? await navigator.mediaDevices.getUserMedia({
+              video: true,
+              audio: false,
+            })
+          : await navigator.mediaDevices.getUserMedia({
+              video: { deviceId: { exact: currentDevice } },
+            });
+      videoRef.current.srcObject = stream;
+      videoRef.current.play();
+    } catch (e) {
+      console.log(e.message);
+    }
+
+    clearPhoto();
+  };
+
+  const handleCanPlay = () => {
     if (!streaming) {
-      height = video.videoHeight / (video.videoWidth / width);
+      height = videoRef.current.videoHeight / (videoRef.current.videoWidth / width);
 
       // Firefox a un bug où la hauteur ne peut pas être lue
       // à partir de la vidéo. On prend des précautions.
@@ -66,54 +75,43 @@ function App() {
         height = width / (4 / 3);
       }
 
-      video.setAttribute("width", width);
-      video.setAttribute("height", height);
-      photo.setAttribute("width", width);
-      photo.setAttribute("height", height);
-      canvas.setAttribute("width", width);
-      canvas.setAttribute("height", height);
-      streaming = true;
+      videoRef.current.setAttribute("width", width);
+      videoRef.current.setAttribute("height", height);
+      photoRef.current.setAttribute("width", width);
+      photoRef.current.setAttribute("height", height);
+      canvasRef.current.setAttribute("width", width);
+      canvasRef.current.setAttribute("height", height);
+      setStreaming(true);
     }
-  },
-  false,
-  );
-  
-  startButton.addEventListener(
-    "click",
-    (e) => {
-      takePicture();
-      e.preventDefault();
-    },
-    false,
-  );
-
-  clearPhoto();
   }
+
+  const handleClik = () => {
+    takePicture();
+  };
 
   const clearPhoto = () => {
-    const context = canvas.getContext("2d");
-      context.fillStyle = "#AAA";
-      context.fillRect(0, 0, canvas.width, canvas.height);
-  
-      const data = canvas.toDataURL("image/png");
-      photo.setAttribute("src", data);
-  }
+    const context = canvasRef.current.getContext("2d");
+    context.fillStyle = "#AAA";
+    context.fillRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+
+    const data = canvasRef.current.toDataURL("image/png");
+    photoRef.current.setAttribute("src", data);
+  };
 
   const takePicture = () => {
-    const context = canvas.getContext("2d");
+    const context = canvasRef.current.getContext("2d");
     if (width && height) {
-      canvas.width = width;
-      canvas.height = height;
-      context.drawImage(video, 0, 0, width, height);
+      canvasRef.current.width = width;
+      canvasRef.current.height = height;
+      context.drawImage(videoRef.current, 0, 0, width, height);
 
-      const data = canvas.toDataURL("image/png");
-      console.log(data)
-      photo.setAttribute("src", data);
+      const data = canvasRef.current.toDataURL("image/png");
+      console.log(data);
+      photoRef.current.setAttribute("src", data);
     } else {
       clearPhoto();
     }
-  }
-
+  };
 
   return (
     <>
@@ -125,30 +123,46 @@ function App() {
         <section className="canvases">
           <div className="video-container">
             <h2>Vidéos en temps Réel</h2>
-            <video preload='none' id="video" width={720} height={600}>Le flux vidéo n&apos;est pas disponible.</video>
+            <video preload="none" id="video" onCanPlay={handleCanPlay} ref={videoRef}>
+              Le flux vidéo n&apos;est pas disponible.
+            </video>
           </div>
           <div className="detection-container">
             <h2>Video : Objets Détectés</h2>
-            <canvas id="video-canvas">
+            <canvas id="video-canvas" ref={canvasRef}>
               {/* L'apércue actuelle de la caméra ici */}
             </canvas>
           </div>
           <div className="detection-container">
             <h2>Image : Objets Détectés</h2>
-            <img id="detection-canvas">{/* La capture ici*/}</img>
+            <img id="detection-canvas" ref={photoRef}>{/* La capture ici*/}</img>
           </div>
         </section>
 
         <section className="controls">
           <div className="choix_camera">
             <label htmlFor="camera-select">Choix de la caméra: </label>
-            <select name="camera" id="camera-select" onChange={(e) => setCurrentDevice(userDevices.filter(device => device.label == e.target.value)[0].deviceId)}>
+            <select
+              name="camera"
+              id="camera-select"
+              onChange={(e) =>
+                setCurrentDevice(
+                  userDevices.filter(
+                    (device) => device.label == e.target.value
+                  )[0].deviceId
+                )
+              }
+            >
               {deviceSelect}
             </select>
           </div>
         </section>
 
-        <button className="screen-button" id="screenshot-button">
+        <button
+          className="screen-button"
+          id="screenshot-button"
+          onClick={handleClik}
+        >
           Captures
         </button>
 
