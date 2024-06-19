46728043016973aa588db91e63d7ce7c7943808f
Steven TAING <taing.steven@outlook.fr>
2024-06-20 00:32:56 +0200

    Add dark mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ThemeProvider } from "./utils/contexts/ThemeContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ThemeProvider>
     <App />
-)
+  </ThemeProvider>
+);
